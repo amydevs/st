72079f05c17def29eac90158a3e5edf97bb9486d
Amy <ctrlalternativedelete@gmail.com>
2021-11-25 18:01:38 +1100

    added bindings

--- config.h
+++ config.h
@@ -208,8 +208,14 @@ static uint forcemousemod = ShiftMask;
  * Internal mouse shortcuts.
  * Beware that overloading Button1 will disable the selection.
  */
+const unsigned int mousescrollincrement = 2;
+const unsigned int mousescrollincrementlarge = 20;
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
+	{ ShiftMask,            Button4, kscrollup,      {.i = mousescrollincrement} },
+	{ ShiftMask,            Button5, kscrolldown,    {.i = mousescrollincrement} },
+	{ ShiftMask|ControlMask,Button4, kscrollup,      {.i = mousescrollincrementlarge} },
+	{ ShiftMask|ControlMask,Button5, kscrolldown,    {.i = mousescrollincrementlarge} },
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
 	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
@@ -235,6 +241,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} }
 };
 
 /*
